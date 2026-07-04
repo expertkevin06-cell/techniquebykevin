43f62cbe7c9cf0b70d61e9a4e08a73581abc1b32
expertkevin06-cell <expertkevin06@gmail.com>
2026-07-04 22:34:40 +0200

    Update BASE_URL to production server address

--- app/src/main/java/com/tonapp/vehicules/RetrofitClient.kt
+++ app/src/main/java/com/tonapp/vehicules/RetrofitClient.kt
@@ -9,7 +9,7 @@ object RetrofitClient {
     // Pour tester en local depuis l'émulateur Android, utilise 10.0.2.2
     // (c'est l'alias spécial de l'émulateur pour "localhost" de ton PC).
     // Sur un vrai téléphone, mets l'adresse publique de ton serveur (ex: https://tonapi.com/)
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://vehicule-backend.onrender.com/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
